5feaa28110bc7d7452e6a22f888cbcb3c6e2ed47
Kao-Wei Yeh <weitheshinobi@gmail.com>
2025-07-17 23:35:50 +0800

    Add `process`, `error`

---
+++ src/process.cpp
@@ -0,0 +1,97 @@
+#include "libsdb/process.hpp"
+
+#include <memory>
+#include <csignal>
+#include <unistd.h>
+#include <bits/signum-arch.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+
+#include "libsdb/error.hpp"
+
+std::unique_ptr<sdb::process> sdb::process::launch(std::filesystem::path path) {
+    pid_t pid;
+    if ((pid = fork()) < 0) {
+        error::send_errno("fork failed");
+    }
+
+    if (pid == 0) {
+        if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
+            error::send_errno("Trace failed");
+        }
+        if (execlp(path.c_str(), path.c_str(), nullptr) < 0) {
+            error::send_errno("exec failed");
+        }
+    }
+
+    std::unique_ptr<process> proc(new process(pid, true));
+    proc->wait_on_signal();
+
+    return proc;
+}
+
+std::unique_ptr<sdb::process> sdb::process::attach(pid_t pid) {
+    if (pid == 0) {
+        error::send("Invalid pid");
+    }
+
+    if (ptrace(PTRACE_ATTACH, pid, nullptr, nullptr) < 0) {
+        error::send_errno("Could not attach process");
+    }
+
+    std::unique_ptr<process> proc(new process(pid, false));
+    proc->wait_on_signal();
+
+    return proc;
+}
+
+sdb::process::~process() {
+    if (pid_ != 0) {
+        int status;
+        if (state_ == process_state::running) {
+            kill(pid_, SIGSTOP);
+            waitpid(pid_, &status, 0);
+        }
+
+        ptrace(PTRACE_DETACH, pid_, nullptr, nullptr);
+        kill(pid_, SIGCONT);
+
+        if (terminate_on_end_) {
+            kill(pid_, SIGKILL);
+            waitpid(pid_, &status, 0);
+        }
+    }
+}
+
+void sdb::process::resume() {
+    if (ptrace(PTRACE_CONT, pid_, nullptr, nullptr) < 0) {
+        error::send_errno("resume failed");
+    }
+    state_ = process_state::running;
+}
+
+sdb::stop_reason::stop_reason(int wait_status) {
+    if (WIFEXITED(wait_status)) {
+        reason = process_state::exited;
+        info = WEXITSTATUS(wait_status);
+    }
+    else if (WIFSIGNALED(wait_status)) {
+        reason = process_state::terminated;
+        info = WTERMSIG(wait_status);
+    }
+    else if (WIFSTOPPED(wait_status)) {
+        reason = process_state::stopped;
+        info = WSTOPSIG(wait_status);
+    }
+}
+
+sdb::stop_reason sdb::process::wait_on_signal() {
+    int wait_status;
+    int options = 0;
+    if (waitpid(pid_, &wait_status, options) < 0) {
+        error::send_errno("wait_on_signal failed");
+    }
+    stop_reason reason(wait_status);
+    state_ = reason.reason;
+    return reason;
+}
--- tools/sdb.cpp
+++ tools/sdb.cpp
@@ -8,10 +8,13 @@
 #include <vector>
 #include <sstream>
 
+#include "libsdb/error.hpp"
+#include "libsdb/process.hpp"
+
 namespace {
     std::vector<std::string> split(std::string_view str, char delimiter) {
         std::vector<std::string> result{};
-        std::stringstream ss {std::string{str}};
+        std::stringstream ss{std::string{str}};
         std::string item;
 
         while (std::getline(ss, item, delimiter)) {
@@ -26,67 +29,73 @@ namespace {
         return std::equal(str.begin(), str.end(), prefix.begin());
     }
 
-    void resume(pid_t pid) {
-        if (ptrace(PTRACE_CONT, pid, nullptr, nullptr) < 0) {
-            std::cerr << "Couldn't continue\n";
-            std::exit(-1);
-        }
-    }
-
-    void wait_on_signal(pid_t pid) {
-        int wait_signal;
-        int options = 0;
-        if (waitpid(pid, &wait_signal, options) < 0) {
-            std::perror("waitpid failed");
-            std::exit(-1);
-        }
-    }
-
-    pid_t attach(int argc, const char **argv) {
-        pid_t pid = 0;
-
+    std::unique_ptr<sdb::process> attach(int argc, const char **argv) {
         if (argc == 3 && argv[1] == std::string_view("-p")) {
-            pid = std::atoi(argv[2]);
-            if (pid <= 0) {
-                std::cerr << "Invalid pid\n";
-                return -1;
-            }
-            if (ptrace(PTRACE_ATTACH, pid, nullptr, nullptr) < 0) {
-                std::perror("Could not attach");
-                return -1;
-            }
-        } else {
-            const char *program_path = argv[1];
-            if ((pid = fork()) < 0) {
-                std::perror("fork failed");
-                return -1;
-            }
-
-            if (pid == 0) {
-                if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
-                    std::perror("Trace failed");
-                    return -1;
-                }
-                if (execlp(program_path, program_path, nullptr) < 0) {
-                    std::perror("Trace failed");
-                    return -1;
-                }
-            }
+            pid_t pid = std::atoi(argv[2]);
+            return sdb::process::attach(pid);
         }
-
-        return pid;
+        const char *program_path = argv[1];
+        return sdb::process::launch(program_path);
     }
 
-    void handle_command(const pid_t pid, const std::string_view line) {
+    void print_stop_reason(const sdb::process &process, sdb::stop_reason reason) {
+        std::cout << "Process " << process.pid() << ' ';
+
+        switch (reason.reason) {
+            case sdb::process_state::exited:
+                std::cout << "exited with status "
+                        << static_cast<int>(reason.info);
+                break;
+            case sdb::process_state::terminated:
+                std::cout << "terminated with signal "
+                        << sigabbrev_np(reason.info);
+                break;
+            case sdb::process_state::stopped:
+                std::cout << "stopped with signal " << sigabbrev_np(reason.info);
+                break;
+            case sdb::process_state::running:
+                break;
+        }
+
+        std::cout << std::endl;
+    }
+
+    void handle_command(std::unique_ptr<sdb::process> &process, const std::string_view line) {
         const auto args = split(line, ' ');
-        for (auto a: args) std::cout << a << " "   ;
-        const auto& command = args[0];
+        const auto &command = args[0];
 
         if (is_prefix(command, "continue")) {
-            resume(pid);
-            wait_on_signal(pid);
+            process->resume();
+            auto reason = process->wait_on_signal();
+            print_stop_reason(*process, reason);
         } else {
             std::cerr << "Unknown command: " << command << "\n";
+        }
+    }
+
+    void main_loop(std::unique_ptr<sdb::process> &process) {
+        char *line = nullptr;
+        while ((line = readline("sdb> ")) != nullptr) {
+            std::string line_str;
+
+            if (line == std::string_view("")) {
+                free(line);
+                if (history_length > 0) {
+                    line_str = history_list()[history_length - 1]->line;
+                }
+            } else {
+                line_str = line;
+                add_history(line);
+                free(line);
+            }
+
+            if (!line_str.empty()) {
+                try {
+                    handle_command(process, line_str);
+                } catch (const sdb::error &err) {
+                    std::cout << err.what() << '\n';
+                }
+            }
         }
     }
 }
@@ -97,31 +106,10 @@ int main(int argc, const char **argv) {
         return -1;
     }
 
-    pid_t pid = attach(argc, argv);
-
-    int wait_status;
-    int options = 0;
-    if (waitpid(pid, &wait_status, options) < 0) {
-        std::perror("waitpid failed");
-    }
-
-    char *line = nullptr;
-    while ((line = readline("sdb> ")) != nullptr) {
-        std::string line_str;
-
-        if (line == std::string_view("")) {
-            free(line);
-            if (history_length > 0) {
-                line_str = history_list()[history_length - 1]->line;
-            }
-        } else {
-            line_str = line;
-            add_history(line);
-            free(line);
-        }
-
-        if (!line_str.empty()) {
-            handle_command(pid, line_str);
-        }
+    try {
+        auto process = attach(argc, argv);
+        main_loop(process);
+    } catch (const sdb::error &err) {
+        std::cerr << err.what() << '\n';
     }
 }
